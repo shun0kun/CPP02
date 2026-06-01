a059f01d73647755ebbbc976743ff5b05a54436f
shunsuke <shun0classic@gmail.com>
2026-06-01 07:01:34 +0000

    a

---
+++ ex03/Point.hpp
@@ -0,0 +1,28 @@
+#ifndef POINT_HPP
+# define POINT_HPP
+
+# include "Fixed.hpp"
+
+class Point
+{
+	public:
+		Point();
+		Point( Point const &other );
+		Point( int const x, int const y );
+		Point( float const x, float const y );
+		Point( Fixed const x, Fixed const y );
+		Point	&operator=( Point const &other );
+		~Point();
+		Fixed	getX() const;
+		Fixed	getY() const;
+
+	private:
+		Fixed const	x_;
+		Fixed const	y_;
+};
+
+Point	operator+( Point const &a, Point const &b );
+Point	operator-( Point const &a, Point const &b );
+Fixed	operator^( Point const &a, Point const &b );
+
+#endif
